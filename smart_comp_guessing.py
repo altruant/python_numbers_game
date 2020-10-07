5974cd240a6569d0bb6d4bd072223a8c0d7fa62a
altruant <noellehamberis@gmail.com>
2020-10-07 09:45:48 -0400

    refactor

--- smart_comp_guessing.py
+++ smart_comp_guessing.py
@@ -2,30 +2,25 @@ from random import randint
 
 
 userPrompt = 'Please enter a number between 0-100\n'
-user = -1
 
-while user < 0 or user > 100:
-    user = int(input(userPrompt))
-    if user < 0:
-        user = int(input(userPrompt))
-    elif user > 100:
-        user = int(input(userPrompt))
+user = int(input(userPrompt))
 
-computer = randint(0,100)
+# computer = randint(0,100)
 count = 0
 
 lower = 0
 upper = 100
-while computer != user:
-    print('The Computer guessed the number ', computer)
+
+while count < 8:
     count += 1
-    if computer > user:
+    computer = (lower + upper)//2
+    print('The Computer guessed the number', computer)
+
+    if computer == user:
+        print('The Computer guessed the number', computer)
+        print('It took the Computer', count, 'guesses to find your number')
+        break
+    elif computer > user:
         upper = computer
     elif computer < user:
         lower = computer
-    computer = (lower+upper)//2
-    if computer == user:
-        count += 1
-        print('The Computer guessed the number ', computer)
-        print('It took the Computer', count, 'guesses to find your number')
-        break
